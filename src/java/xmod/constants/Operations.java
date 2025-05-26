212d5a6c6816d76e472742e3371435f393f7bd15
Emily Lindsay-Smith <emily.lindsay-smith@ling-phil.ox.ac.uk>
2025-05-26 12:34:44 +0100

    Create constants to define actions, operations and basic typesetting across classes

---
+++ src/java/xmod/constants/Operations.java
@@ -0,0 +1,20 @@
+package xmod.constants;
+
+/**
+ * Constants defining behaviour and label of the buttons in the main GUI
+ */
+public final class Operations{
+    private Operations(){
+        //restrict instantiation
+    }
+
+    public static final String LOAD_TMS = "LOAD TMS FILE";
+    public static final String LOAD_WAV = "LOAD WAV FILE";
+    public static final String RUN_EXP = "RUN EXPERIMENT";
+    public static final String MONITOR_ON = "TURN MONITORS ON";
+    public static final String MONITOR_OFF = "TURN MONITORS OFF";
+    public static final String CHECK_CONNECTION = "CHECK CONNECTION";
+    public static final String CONTROLLER_INFO = "CONTROLLER INFO";
+    public static final String CHECK_FONT = "CHANGE FONT";
+    public static final String CLOSE_XMOD = "CLOSE XMOD";
+}
