c21f73d1c5015285ad348eb48ec7b167b9fa375d
Emily Lindsay-Smith <emily.lindsay-smith@ling-phil.ox.ac.uk>
2025-05-26 12:37:14 +0100

    Create README and scripts to make running and testing easier'

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#use this script from xmod/ to compile and run the program
+
+javac ./src/java/xmod/Xmod.java src/java/xmod/*/*.java -cp "lib/*" -d bin/ \
+	 && cd bin \
+	 && java -classpath .:../lib/jSerialComm-2.11.0.jar xmod.Xmod 
