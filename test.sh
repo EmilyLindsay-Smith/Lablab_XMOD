4093c89d2eda32a264b5be4e67029c3388d24897
Emily Lindsay-Smith <emily.lindsay-smith@ling-phil.ox.ac.uk>
2025-06-04 13:34:36 +0100

    Make minor changes to runfiles

--- test.sh
+++ test.sh
@@ -2,5 +2,14 @@
 
 #Use this script from xmod/ to compile and run the tests
 
-javac -d out ./test/*.java ./src/java/xmod/*/*.java -cp "./lib/*" \
-	&& java -jar ./lib/junit-platform-console-standalone-1.8.2.jar -cp out --scan-classpath
+javac -d out ./test/*.java ./src/java/xmod/*/*.java -cp "lib/*" \
+	&& java -jar ./lib/junit-platform-console-standalone-1.8.2.jar -cp ./lib/jSerialComm-2.11.0.jar  -cp out --scan-classpath
+
+
+#	&& java -jar ./lib/junit-platform-console-standalone-1.8.2.jar -cp out --scan-classpath
+
+
+
+#javac ./src/java/xmod/Xmod.java src/java/xmod/*/*.java -cp "lib/*" -d bin/ \
+#	 && cd bin \
+#	 && java -classpath .:../lib/jSerialComm-2.11.0.jar xmod.Xmod 
