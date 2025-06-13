21d766c8ef57bc28f700c40420d2d1c0b684fa27
Emily Lindsay-Smith <emily.lindsay-smith@ling-phil.ox.ac.uk>
2025-06-13 15:51:49 +0100

    Add javadoc comments to Actions, Operations and Typesetting

--- src/java/xmod/constants/Actions.java
+++ src/java/xmod/constants/Actions.java
@@ -4,11 +4,16 @@ public final class Actions {
     private Actions() {
         //restrict instantiation
     }
-    public static final String ERROR = "ERROR";
-    public static final String UPDATE_CONNECTION = "UPDATE_CONNECTION";
+    /** Update Reporter with new information (including errors). */
+    public static final String UPDATE = "UPDATE";
+    /** Button Operation (options defined in constnats.Operations.java). */
     public static final String OPERATION = "OPERATION";
+    /** Abort the experiment mid-run. */
     public static final String ABORT_EXPERIMENT = "ABORT_EXPERIMENT";
+    /** Change the font. */
     public static final String UPDATE_FONT = "UPDATE_FONT";
+    /** End the experiment. */
     public static final String FINISH_EXPERIMENT = "FINISH_EXPERIMENT";
+    /** Close the Xmod application. */
     public static final String CLOSE_XMOD = "CLOSE XMOD";
 }
--- src/java/xmod/constants/Typesetting.java
+++ src/java/xmod/constants/Typesetting.java
@@ -4,8 +4,9 @@ public final class Typesetting {
     private Typesetting() {
         //restrict instantiation
     }
-
+    /** Tab. */
     public static final String TAB = "\t";
+    /** New Line. */
     public static final String CR = System.getProperty("line.separator");
 
 }
