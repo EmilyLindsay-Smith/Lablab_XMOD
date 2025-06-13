1b37b51fa885c69e653881f2807f9799eb76a050
Emily Lindsay-Smith <emily.lindsay-smith@ling-phil.ox.ac.uk>
2025-06-13 10:26:01 +0100

    Add linter

---
+++ checkstyle.sh
@@ -0,0 +1,16 @@
+!/bin/bash
+
+if [ $# -ne 1]; then
+    echo "Usage: checkstyle.sh directory"
+    echo "e.g. ./checkstyle.sh test"
+    echo "e.g. ./checkstyle.sh serial"
+fi
+
+
+if [ $1="test" ]; then
+    dirToCheck=$1
+else
+    dirToCheck="src/java/xmod/$1"
+fi
+
+java -jar lib/checkstyle-10.25.0-all.jar -c lib/sun_checks.xml $dirToCheck
