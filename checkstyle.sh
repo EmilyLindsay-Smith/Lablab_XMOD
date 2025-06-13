05d059597bd82db31d3bef8497a87bc72cb69d54
Emily Lindsay-Smith <emily.lindsay-smith@ling-phil.ox.ac.uk>
2025-06-13 10:28:19 +0100

    Add linter

--- checkstyle.sh
+++ checkstyle.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 if [ $# -ne 1]; then
     echo "Usage: checkstyle.sh directory"
@@ -7,10 +7,13 @@ if [ $# -ne 1]; then
 fi
 
 
-if [ $1="test" ]; then
+if [ $1 = "test" ]; then
     dirToCheck=$1
 else
     dirToCheck="src/java/xmod/$1"
 fi
+
+echo $dirToCheck
+
 
 java -jar lib/checkstyle-10.25.0-all.jar -c lib/sun_checks.xml $dirToCheck
