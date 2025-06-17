995220cab8761797d4ff08c5addf05dc7fa75042
Emily Lindsay-Smith <emily.lindsay-smith@ling-phil.ox.ac.uk>
2025-06-17 15:35:07 +0100

    Fix bugs due to change in attribute visibility; small linting changes

--- test/ExperimentLoaderFurtherTest.java
+++ test/ExperimentLoaderFurtherTest.java
@@ -5,16 +5,24 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.List;
+
+import java.io.IOException;
+
 class ExperimentLoaderFurtherTest {
     // Tests for Parsing Files Correctly
+        /** Experiment loader object. */
         private ExperimentLoader e;
+        /** TMS file used for tests. */
+        private String testFile = "./test/testFiles/charlie_short.tms";
 
         @BeforeEach
         void parseFile() {
-            String file = "./test/testFiles/charlie_short.tms";
             e = new ExperimentLoader();
             try {
-                Boolean loaded = e.loadFile(file);
+                Boolean loaded = e.loadFile(testFile);
                 e.parseFile();
                 Assertions.assertEquals(true, loaded,
                      "Valid file was not loaded");
@@ -27,26 +35,34 @@ class ExperimentLoaderFurtherTest {
         @DisplayName("Check Arrays")
         @Test
         public void checkArrays() {
-            //Note difference from getarraySize() due to getTMSInfo() code
-            int arraySize = (int) Math.floor((e.tmsFileLines.size() + 1 ) / 2);
-            if (arraySize / 2 == 0) {
-                arraySize = arraySize - 1;
+            int arrSize = 0;
+            try {
+                List<String> fStream = Files.readAllLines(
+                                                Paths.get(this.testFile));
+                arrSize = (int) (fStream.size() - 2) / 2;
+                if (arrSize / 2 == 0) {
+                    arrSize = arrSize - 1;
+                }
+            } catch (IOException exc) {
+                Assertions.assertEquals(false, true,
+                "Could not read testfile needed to calculate number of lines");
             }
-            if (e.tmsLoaded) {
-                Assertions.assertEquals(arraySize, e.tReactionTimeout.length,
+
+            if (e.getTMSLoaded()) {
+                Assertions.assertEquals(arrSize, e.getTReactionTimeout().length,
                     "e.tReactionTimeout is wrong size");
-                Assertions.assertEquals(arraySize, e.tReactionOffset.length,
+                Assertions.assertEquals(arrSize, e.getTReactionOffset().length,
                     "e.tReactionOffset is wrong size");
-                Assertions.assertEquals(arraySize, e.tMonitorOn.length,
+                Assertions.assertEquals(arrSize, e.getTMonitorOn().length,
                     "e.tMonitorOn is wrong size");
-                Assertions.assertEquals(arraySize, e.tMonitorOff.length,
+                Assertions.assertEquals(arrSize, e.getTMonitorOff().length,
                     "e.tMonitorOff is wrong size");
-                Assertions.assertEquals(arraySize, e.codingArray.length,
+                Assertions.assertEquals(arrSize, e.getCodingArray().length,
                     "e.codingArray is wrong size");
-                Assertions.assertEquals(arraySize, e.screenItems.length,
+                Assertions.assertEquals(arrSize, e.getScreenItems().length,
                     "e.screenItems is wrong size");
             } else {
-                Assertions.assertEquals(true, e.tmsLoaded,
+                Assertions.assertEquals(true, e.getTMSLoaded(),
                     "parseFile() not working correctly");
             }
         }
@@ -54,17 +70,18 @@ class ExperimentLoaderFurtherTest {
         @DisplayName("Check Extracted Info")
         @Test
         public void checkExtractedInfo() {
-            if (e.tmsLoaded) {
+            if (e.getTMSLoaded()) {
             String[] expectedItems = {"5", "4", "3", "2", "1", "darsh",
                          "unskilled", "scarble", "perfume", "unfixed"};
-            Assertions.assertEquals(expectedItems.length, e.screenItems.length,
+            String[] screenItems = e.getScreenItems();
+            Assertions.assertEquals(expectedItems.length, screenItems.length,
                  "Wrong number of expected items");
             for (int i = 0; i < expectedItems.length; i++) {
-                Assertions.assertEquals(expectedItems[i], e.screenItems[i],
+                Assertions.assertEquals(expectedItems[i], screenItems[i],
                     "Wrong expected item in screenItems");
             }
             } else {
-                Assertions.assertEquals(true, e.tmsLoaded,
+                Assertions.assertEquals(true, e.getTMSLoaded(),
                     "parseFile() not working correctly");
             }
         }
--- test/ExperimentLoaderTest.java
+++ test/ExperimentLoaderTest.java
@@ -1,27 +1,18 @@
 package xmod.experimenter;
 
-import xmod.status.ObjectReport;
-import xmod.constants.Actions;
 import xmod.utils.TestListener;
-
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.junit.jupiter.api.Test;
 
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeEvent;
+public final class ExperimentLoaderTest {
 
-public class ExperimentLoaderTest {
-
+    /** Test ensuring valid file is loaded. */
     @DisplayName("Load valid files")
     @ParameterizedTest
     @ValueSource(strings = {"./test/testFiles/charlie_short.tms"})
-    public void loadValidFiles(String file) {
+    public void loadValidFiles(final String file) {
         try {
             ExperimentLoader e = new ExperimentLoader();
             TestListener tester = new TestListener();
@@ -30,16 +21,17 @@ public class ExperimentLoaderTest {
             Assertions.assertEquals(true, loaded, "Valid file was not loaded");
             Assertions.assertEquals(false, tester.checkUpdate(),
                  "ExperimentLoader sent update unexpectedly");
-        } catch (Exception exc){
+        } catch (Exception exc) {
             Assertions.assertEquals(1, 0, "File not loaded for test");
         }
      }
 
+    /** Test ensuring invalid file is not loaded. */
     @DisplayName("Fail to load invalid files")
     @ParameterizedTest
     @ValueSource(strings = {"./test/testFiles/charlie_short.txt",
                             "./test/testFiles/charlie_shorts.tms"})
-    public void loadValidFiles2(String file) {
+    public void loadValidFiles2(final String file) {
         try {
             ExperimentLoader e = new ExperimentLoader();
             TestListener tester = new TestListener();
@@ -48,7 +40,7 @@ public class ExperimentLoaderTest {
             Assertions.assertEquals(false, loaded, "Invalid file was loaded");
             Assertions.assertEquals(true, tester.checkUpdate(),
                  "ExperimentLoader should have sent an update");
-        } catch (Exception exc){
+        } catch (Exception exc) {
             Assertions.assertEquals(1, 1, "Invalid file not loaded for test");
         }
     }
