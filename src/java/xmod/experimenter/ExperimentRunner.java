2469820807fb5e38694e06488969251b4513d94d
Emily Lindsay-Smith <emily.lindsay-smith@ling-phil.ox.ac.uk>
2025-06-18 14:14:46 +0100

    Update ExperimentRunner to integrate ExperimentResulter.java git add src/java/xmod/experimenter/ExperimentResulter.java

--- src/java/xmod/experimenter/ExperimentRunner.java
+++ src/java/xmod/experimenter/ExperimentRunner.java
@@ -32,7 +32,7 @@ public class ExperimentRunner implements PropertyChangeListener {
     /** Experiment Loader to load and parse the tms file. */
     private ExperimentLoader expLoader;
     /** ExperimentResulter to parse the results and print to file. */
-    //private ExperimentResulter expResulter;
+    private ExperimentResulter expResulter;
     /** PCS to listen for updates from ExperimentLoader. */
     private PropertyChangeSupport pcs;
 
@@ -98,8 +98,16 @@ public class ExperimentRunner implements PropertyChangeListener {
             this.tMonitorOff = this.expLoader.getTMonitorOff();
             this.codingArray = this.expLoader.getCodingArray();
             this.screenItems = this.expLoader.getScreenItems();
-
-            //this.expResulter = new ExperimentResulter(this.expLoader);
+            int[] tReactionOffset = this.expLoader.getTReactionOffset();
+            String codehead = this.expLoader.getCodehead();
+            this.expResulter = new ExperimentResulter(filename,
+                                                        this.expLength,
+                                                        tReactionOffset,
+                                                        codehead,
+                                                        this.codingArray,
+                                                        this.screenItems
+                                                    );
+            this.expResulter.addObserver(this);
             updateStatus(Responses.FILE_LOAD_SUCCESS + filename,
                         "Total number of trials: " + this.expLength,
                         "", "", ReportLabel.TMS);
@@ -122,6 +130,12 @@ public class ExperimentRunner implements PropertyChangeListener {
 
     }
 
+    /** Returns true if experiment loaded.
+     * @return this.experimentLoaded;
+     */
+    public Boolean isExperimentLoaded() {
+        return this.experimentLoaded;
+    }
     /** Listen for updates from ExperimentLoader.
      * @param evt property change event
      */
@@ -192,7 +206,7 @@ public class ExperimentRunner implements PropertyChangeListener {
                         "Cannot begin experiment as not connected"
                         + " to controller box",
                         "Please check serial port connection",
-                        "", ReportLabel.CONNECTION);
+                        "", ReportLabel.STATUS);
             return;
         }
         // Set flag to true
@@ -216,9 +230,9 @@ public class ExperimentRunner implements PropertyChangeListener {
             // complete set of reactions is 36 bytes
             byte[] reaction = this.serialPort.receiveChunk(this.REACTION_SET);
             // collects button pressed and reaction time for all 16 boxes
-            //this.expResulter.collectTrialResults(reaction, trialIndex);
+            this.expResulter.collectTrialResults(reaction, trialIndex);
         }
-        //this.expResulter.printResults();
+        this.expResulter.printResults();
         if  (this.running) {
             //tells main Xmod instance experiment is finished
             //so it can change window views etc
