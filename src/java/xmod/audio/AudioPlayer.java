ebfbb3522a13a82c9852b2aada1d18a6e4800a37
Emily Lindsay-Smith <emily.lindsay-smith@ling-phil.ox.ac.uk>
2025-06-19 17:18:56 +0100

    Create AudioPlayer class

---
+++ src/java/xmod/audio/AudioPlayer.java
@@ -0,0 +1,300 @@
+package xmod.audio;
+
+import xmod.constants.Actions;
+import xmod.status.ObjectReport;
+import xmod.status.ReportCategory;
+import xmod.status.ReportLabel;
+import xmod.utils.Utils;
+//import java.io.File;
+
+import java.nio.file.Files;
+//import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+// For reporting errors to GUI
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+/**
+ * AudioPlayer class loads and plays the audio file.
+ * @author ELS
+ * @version 2.0
+ * @since 2025-02-13
+ * NOTES:
+ */
+
+public class AudioPlayer extends Thread {
+    /** PCS to handle sending updates. */
+    private PropertyChangeSupport pcs;
+    /** Audio file. */
+    private String audioFilePath;
+    /** Buffer size. */
+    private static final int BUFFER_SIZE = 4096;
+    /** Whether the audio has been successfully loaded. */
+    private Boolean audioLoaded = false;
+    /** Whether the audio is currently playing. */
+    private Boolean playAudio = false;
+    /** SourceDataLine. */
+    private SourceDataLine sourceDataLine;
+    /** AudioInputStream. */
+    private AudioInputStream audioStream;
+    /** InputStream. */
+    private InputStream inputStream;
+    /** InputStream. */
+    private InputStream originalInputStream;
+    /** AudioFormat. */
+    private AudioFormat audioFormat;
+    /** Dataline info. */
+    private DataLine.Info info;
+
+    /**
+     * AudioLoader Constructor.
+     * @param audioFile name & path of audio file
+     */
+    public AudioPlayer(final String audioFile) {
+        pcs = new PropertyChangeSupport(this);
+        if (null != audioFile && isAudioValid(audioFile)) {
+            this.audioFilePath = audioFile;
+            setUpPlayer();
+        }
+    }
+
+    /**
+     * Validates audio file - checks if filename ends in .wav and file exists.
+     * @param audioFile
+     * @return boolean if valid
+     */
+    private Boolean isAudioValid(final String audioFile) {
+        if (null == audioFile) {
+            return false;
+        }
+        return (Utils.fileHasExtension(audioFile, "wav")
+                && Utils.fileExists(audioFile));
+    }
+
+     /**
+     * Loads audio file.
+     * ELS CHECK LIKELY EXCEPTIONS TO CATCH THEM
+     */
+    private void setUpPlayer() {
+        if (null == this.audioFilePath || !isAudioValid(this.audioFilePath)) {
+            return;
+        }
+        try {
+            this.originalInputStream = Files.newInputStream(
+                                                Paths.get(this.audioFilePath));
+            this.inputStream = new BufferedInputStream(
+                                                    this.originalInputStream);
+        } catch (Exception e) {
+            String stackTrace = Utils.getStackTrace(e);
+            updateStatus("", "Could not load audio file due to error",
+                            "", stackTrace);
+            return;
+        }
+
+        try {
+            this.audioStream = AudioSystem.getAudioInputStream(
+                                                            this.inputStream);
+        } catch (UnsupportedAudioFileException e) {
+            String stackTrace = Utils.getStackTrace(e);
+            updateStatus("", "Could not load audio file due to "
+                            + "unsupported audio file error",
+                            "", stackTrace);
+            return;
+        } catch (Exception e) {
+            String stackTrace = Utils.getStackTrace(e);
+            updateStatus("", "Could not load audio file due to error",
+                            "", stackTrace);
+            return;
+        }
+
+        this.audioFormat = this.audioStream.getFormat();
+        this.info = new DataLine.Info(SourceDataLine.class, this.audioFormat);
+
+        this.audioLoaded = true; // flag updated
+    }
+
+
+    /** Start thread to play audio file. */
+    public void play() {
+        if (this.getState() == Thread.State.NEW) {
+            this.setName("AUDIO PLAYER");
+            new Thread(new Runnable() {
+                public void run() {
+                    playAudio();
+                    return;
+                };
+            }, "AUDIO PLAYER").start();
+        } else if (this.getState() == Thread.State.TERMINATED) {
+            try {
+                this.setUpPlayer();
+                new Thread(new Runnable() {
+                public void run() {
+                    playAudio();
+                    return;
+                };
+            }, "AUDIO PLAYER").start();
+            } catch (Exception e) {
+                String stackTrace = Utils.getStackTrace(e);
+                updateStatus("", "Could not play audio due to thread issue",
+                            "", stackTrace);
+            }
+        }
+    }
+    /**
+     * Plays audio file.
+     */
+    private void playAudio() {
+        if (!this.audioLoaded) {
+            updateStatus("", "Could not play audio as not loaded",
+                            "", "");
+            return;
+        }
+        this.playAudio = true;
+        try {
+            this.sourceDataLine = (SourceDataLine) AudioSystem.getLine(
+                                                                this.info);
+            this.sourceDataLine.open(audioFormat);
+        } catch (LineUnavailableException e) {
+            String stackTrace = Utils.getStackTrace(e);
+            updateStatus("", "Could not play audio file due to error",
+                        "", stackTrace);
+            return;
+        } catch (Exception e) {
+            String stackTrace = Utils.getStackTrace(e);
+            updateStatus("", "Could not play audio file due to error",
+                        "", stackTrace);
+            return;
+        }
+
+        this.sourceDataLine.start();
+
+        byte[] bufferBytes = new byte[this.BUFFER_SIZE];
+        int readBytes = -1;
+        try {
+            while (this.playAudio
+                && (readBytes = this.audioStream.read(bufferBytes)) != -1) {
+                    this.sourceDataLine.write(bufferBytes, 0, readBytes);
+                }
+        } catch (IOException e) {
+            String stackTrace = Utils.getStackTrace(e);
+            updateStatus("", "Could not play audio file due to error",
+                        "", stackTrace);
+            return;
+        } catch (Exception e) {
+            String stackTrace = Utils.getStackTrace(e);
+            updateStatus("", "Could not play audio file due to error",
+                        "", stackTrace);
+            return;
+        }
+        stopAudio();
+
+    }
+
+    /**
+     * Stops the audio playing.
+     */
+    public void stopAudio() {
+        this.playAudio = false; // this stops playAudio()
+        sourceDataLine.drain();
+        sourceDataLine.close();
+
+        try {
+            audioStream.close();
+        } catch (IOException e) {
+            String stackTrace = Utils.getStackTrace(e);
+                updateStatus("", "Could not stop audio file due to error",
+                            "", stackTrace);
+            return;
+        } catch (Exception e) {
+            String stackTrace = Utils.getStackTrace(e);
+                updateStatus("", "Could not stop audio file due to error",
+                            "", stackTrace);
+            return;
+        }
+    }
+
+
+    /** Returns length of audio file in HH:MM:SS format.
+     * @return audio file duration
+     */
+    public String getAudioLength() {
+        if (null == this.audioLoaded) {
+            return "No audio file has been loaded";
+        }
+        int timeDivisor = 60;
+        int duration = this.getAudioLengthInSeconds();
+        int seconds = duration % timeDivisor;
+        int minutes = duration / timeDivisor;
+        int hours = minutes / timeDivisor;
+        minutes = minutes % timeDivisor;
+        String durationString = hours + ":" + minutes + ":" + seconds;
+        return durationString;
+    }
+
+    /**
+     * Calculates the duration in seconds.
+     * Note: this truncates any fractional seconds rather than rounding down/up
+     * @return audio length in seconds
+     */
+    private int getAudioLengthInSeconds() {
+        if (null != this.audioStream && null != this.audioFormat) {
+            long frameLength = this.audioStream.getFrameLength();
+            float frameRate = this.audioFormat.getFrameRate();
+
+            //calculate duration in sections
+            // note the typecasting will just truncate the float
+            return (int) (frameLength / frameRate);
+        } else {
+            return -1;
+        }
+    }
+
+    /**
+     * Send updates to main Xmod.java.
+     * @param newStatus status
+     * @param newMessage message
+     * @param newAdvice advice
+     * @param newStackTrace any stack trace
+     */
+    private void updateStatus(final String newStatus,
+                                final String newMessage,
+                                final String newAdvice,
+                                final String newStackTrace) {
+        ObjectReport report = new ObjectReport(ReportLabel.AUDIO);
+        if (newStatus != "") {
+            report.updateValues(ReportCategory.STATUS, newStatus);
+        }
+        if (newMessage != "") {
+            report.updateValues(ReportCategory.MESSAGE, newMessage);
+        }
+        if (newAdvice != "") {
+            report.updateValues(ReportCategory.ADVICE, newAdvice);
+        }
+        if (newStackTrace != "") {
+            report.updateValues(ReportCategory.STACKTRACE, newStackTrace);
+        }
+        pcs.firePropertyChange(Actions.UPDATE, null, report);
+        return;
+    }
+
+    /**
+     * Used in Xmod.java to allow the controller to listen for pcs.
+     * @param l listener i.e. Xmod.java
+     */
+    public void addObserver(final PropertyChangeListener l) {
+        pcs.addPropertyChangeListener(Actions.UPDATE, l);
+    }
+}
